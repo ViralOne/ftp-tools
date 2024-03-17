b3e543770c366e158b65c9c87831f9ba54a46a4e
Mocanu Marian <viralnico@protonmail.com>
2024-03-17 13:12:54 +0200

    Optimize FTP download script

--- ftp_downloader.py
+++ ftp_downloader.py
@@ -1,8 +1,10 @@
 import os
-from ftplib import FTP
-import time
 import json
+import time
 from concurrent.futures import ThreadPoolExecutor
+from ftplib import FTP
+from urllib.parse import quote
+from pathlib import Path
 
 def load_ftp_servers(file_path):
     with open(file_path, 'r') as file:
@@ -11,66 +13,40 @@ def load_ftp_servers(file_path):
 
 def download_from_ftp(host, username, password, local_folder):
     try:
-        # Connect to FTP server
-        ftp = FTP(host)
-        print(f"Connected to {host}")
-        ftp.login(username, password)
+        with FTP(host) as ftp:
+            print(f"Connected to {host}")
+            ftp.login(username, password)
 
-        # Create a local directory for the host if it doesn't exist
-        host_folder = os.path.join(local_folder, host)
-        os.makedirs(host_folder, exist_ok=True)
+            host_folder = Path(local_folder) / host
+            host_folder.mkdir(parents=True, exist_ok=True)
 
-        # Change to the remote directory
-        ftp.cwd('/')
+            ftp.cwd('/')
 
-        # List files and download each one
-        files = ftp.nlst()
-        for file in files:
-            if file == ".ftpquota" or file == "." or file == "..":
-                continue
-            valid_file_name = file.replace('<', '').replace('>', '').replace(':', '').replace('"', '').replace('/', '').replace('\\', '').replace('|', '').replace('?', '').replace('*', '')
-            local_file = os.path.join(host_folder, valid_file_name)
+            files = ftp.nlst()
+            for file in files:
+                if file in (".ftpquota", ".", ".."):
+                    continue
+                local_file = host_folder / quote(file)
 
-            # Get the size of the file
-            file_size = ftp.size(file)
-
-            # Check if the file size is greater than 0 before downloading
-            if file_size > 0:
-                with open(local_file, "wb") as fileu:
-                    ftp.retrbinary(f"RETR {file}", fileu.write)
-                    ftp.delete(file)
-                print(f'Downloaded locally and removed from host: {file}')
-                time.sleep(1)
-            else:
-                print(f'Skipping download of 0KB file: {file}')
-
-        # Close FTP connection
-        ftp.quit()
+                file_size = ftp.size(file)
+                if file_size > 0:
+                    with open(local_file, "wb") as fileu:
+                        ftp.retrbinary(f"RETR {file}", fileu.write)
+                        ftp.delete(file)
+                    print(f'Downloaded locally and removed from host: {file}')
+                    time.sleep(1)
+                else:
+                    print(f'Skipping download of 0KB file: {file}')
     except Exception as e:
         print(f"Error: {e}")
 
 def main():
-    # Load FTP server credentials from file
     ftp_servers = load_ftp_servers("servers.json")
-
-    # Local folder to save the files
     local_folder = "downloads"
 
-    # Download mode: Set to True for periodic job, False for one-time job
-    periodic_download = False
-
-    # Interval in minutes for periodic job
-    interval_minutes = 30
-
-    # Download from each FTP server in parallel
     with ThreadPoolExecutor() as executor:
         for server in ftp_servers:
             executor.submit(download_from_ftp, server["host"], server["username"], server["password"], local_folder)
-
-    if periodic_download:
-        # Sleep for the specified interval
-        print(f"Sleeping for {interval_minutes} minutes...")
-        time.sleep(interval_minutes * 60)
 
 if __name__ == "__main__":
     main()
