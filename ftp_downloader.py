09c26d70b1562391b4d5631354e88e1e4ee9b266
Mocanu Marian <viralnico@protonmail.com>
2024-03-28 00:40:31 +0200

    add flag for removing after download

--- ftp_downloader.py
+++ ftp_downloader.py
@@ -25,10 +25,12 @@ def parse_arguments():
     parser.add_argument('file_path', nargs='?', default='servers.json', type=str, help='Path to the JSON file containing FTP server details')
     parser.add_argument('-d', '--download-folder', type=str, default='downloads', help='Local folder to save downloaded files (default: downloads)')
     parser.add_argument('-p', '--periodic', action='store_true', help='Enable periodic checking')
+    parser.add_argument('-r', '--remove', action='store_true', help='Remove file after being downloaded')
     parser.add_argument('-i', '--interval', type=int, default=30, help='Interval in minutes for periodic download (default: 30)')
     return parser.parse_args()
 
 def download_from_ftp(host, username, password, local_folder):
+    args = parse_arguments()
     if not is_host_reachable(host):
         print(f"Host {host} is unreachable. Skipping download.")
         return
@@ -51,12 +53,15 @@ def download_from_ftp(host, username, password, local_folder):
                 if file_size > 0:
                     with open(local_file, "wb") as fileu:
                         ftp.retrbinary(f"RETR {file}", fileu.write)
-                        ftp.delete(file)
-                    print(f'Downloaded locally and removed from {host}: {file}')
+                        if args.remove:
+                            ftp.delete(file)
+                            print(f'Downloaded locally and removed from {host}: {file}')
+                    print(f'Downloaded locally from {host}: {file}')
                     time.sleep(1)
                 else:
-                    ftp.delete(file)
-                    print(f'Removed 0KB file: {file}')
+                    if args.remove:
+                        ftp.delete(file)
+                        print(f'Removed 0KB file: {file}')
     except Exception as e:
         print(f"Error: {e} - {host}")
 
